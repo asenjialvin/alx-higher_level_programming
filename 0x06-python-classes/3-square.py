c77c85047b905ef67153cce53dc1c7357bbd70c0
asenjialvin <asenjialvin@gmail.com>
2023-12-19 11:37:27 -0800

    Added task 3

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+''' Module for the Square definition'''
+
+
+class Square:
+    ''' Defines a new square '''
+    def __init__(self, size=0):
+        ''' Constructor.
+        Args:
+            size: width and height of the Square
+        '''
+        if isinstance(size, int):
+            self.__size = size
+        else:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        ''' Calculates the area of Object Square.
+        Return:
+            the current square area
+        '''
+        return (self.__size * self.__size)
