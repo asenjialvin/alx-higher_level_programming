32606d238fd8aaa848395a4ba0deee2eb0b52e54
asenjialvin <asenjialvin@gmail.com>
2024-05-04 14:28:40 -0700

    added all tasks

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,23 +1,9 @@
 #!/usr/bin/node
 
 const fs = require('fs');
-const filePath = process.argv[2];
+const file = process.argv[2];
 const string = process.argv[3];
 
-function writeToFile(filePathe, string) {
-
-        fs.writeFile(filePathe, string,  'utf8',  (error) => {
-
-                if (error) {
-                  console.log(error);
-                }
-
-                console.log(`Content has been written to: ${filePath}`);
-
-
-        })
-
-}
-
-
-writeToFile(filePath, string);
+fs.writeFile(file, string, 'utf-8', function (err) {
+  if (err) console.log(err);
+});
