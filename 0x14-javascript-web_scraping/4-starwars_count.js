6f41393dacdbb4783d7b7138c5d9ff959d258c31
asenjialvin <asenjialvin@gmail.com>
2024-05-04 14:39:02 -0700

    added all tasks

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -2,28 +2,23 @@
 
 const request = require('request');
 const url = process.argv[2];
-const SUBSTRING_TO_FIND = '18';
 
 request(url, function (err, response, body) {
-    if (err) {
-        console.error('Error occurred:', err);
-    } else if (response.statusCode === 200) {
-        try {
-            const films = JSON.parse(body).results;
-            let count = 0;
-            for (const film of films) {
-                const filmChars = film.characters;
-                for (const character of filmChars) {
-                    if (character.includes(SUBSTRING_TO_FIND)) {
-                        count++;
-                    }
-                }
-            }
-            console.log('Count of characters with ID containing', SUBSTRING_TO_FIND + ':', count);
-        } catch (error) {
-            console.error('Error parsing JSON:', error);
+  if (err) {
+    console.log(err);
+  } else if (response.statusCode === 200) {
+    const films = JSON.parse(body).results;
+    let count = 0;
+    for (const filmIndex in films) {
+      const filmChars = films[filmIndex].characters;
+      for (const charIndex in filmChars) {
+        if (filmChars[charIndex].includes('18')) {
+          count++;
         }
-    } else {
-        console.error('An error occurred. Status code:', response.statusCode);
+      }
     }
+    console.log(count);
+  } else {
+    console.log('An error occured. Status code: ' + response.statusCode);
+  }
 });
